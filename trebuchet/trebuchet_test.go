8a12e704769513ab475bcd3537f9bc4b99a7015a
ericmariot <eric.mariot@betrybe.com>
2024-05-01 20:00:00 -0300

    rename test and add a test for part two

--- trebuchet/trebuchet_test.go
+++ trebuchet/trebuchet_test.go
@@ -2,9 +2,24 @@ package trebuchet
 
 import "testing"
 
-func TestFirstItem(t *testing.T) {
-	filePath := "./input_test.txt"
+func TestCalibrationValueFromFile_CorrectSum_PartOne(t *testing.T) {
+	filePath := "./input_test_one.txt"
 	want := 142
+
+	result, err := CalibrationValueFromFile(filePath)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+		return
+	}
+
+	if result != want {
+		t.Errorf("Expected: %v, got: %v", want, result)
+	}
+}
+
+func TestCalibrationValueFromFile_CorrectSum_PartTwo(t *testing.T) {
+	filePath := "./input_test_two.txt"
+	want := 281
 
 	result, err := CalibrationValueFromFile(filePath)
 	if err != nil {
